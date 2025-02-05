5cfd8914d4158600e72a93f303e9783ab2e90fe1
raminonantenainajospin@gmail.com <raminonantenainajospin@gmail.com>
2025-02-05 01:44:32 +0100

    correction responsive section skills

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Skill({ title, skills }) {
   return (
     <div>
-      <h2 className="text-myRose text-3xl font-bold">{title}</h2>
-      <ul className="flex gap-5 flex-wrap justify-center">
+      <h2 className="text-myRose text-3xl font-bold sm:text-start">{title}</h2>
+      <ul className="flex gap-5 flex-wrap justify-center sm:flex-col sm:items-baseline">
         {skills.map((skill, index) => (
           <li key={index} className="flex items-center gap-2">
             <span className="text-lg">{skill.name}</span>
